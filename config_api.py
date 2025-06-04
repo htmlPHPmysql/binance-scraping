2cf76f44ca12b9cd5267dff5c9a2c6698fed6aff
unknown <kolya.ubd2.0@gmail.com>
2025-06-04 15:53:54 +0300

    SQL functions were replaced to functions_sql.py

---
+++ config_api.py
@@ -0,0 +1,12 @@
+# config_api.py
+
+none = 0
+API_URL = "https://www.binance.com/bapi/futures/v1/friendly/future/copy-trade/home-page/query-list"
+TOTAL_ITEMS = 20
+PAGE_SIZE = 18
+BATCH_SIZE = 50
+
+
+trader_links_batch = []
+trader_metrics_batch = []
+trader_performance_batch = []
