debbee164d1b3720696f86bd97ee57f15e5e6284
unknown <kolya.ubd2.0@gmail.com>
2025-06-06 11:22:26 +0300

    Added new file scraping_mock.py

--- config_api.py
+++ config_api.py
@@ -5,7 +5,8 @@ API_URL = "https://www.binance.com/bapi/futures/v1/friendly/future/copy-trade/ho
 TOTAL_ITEMS = 20
 PAGE_SIZE = 18
 BATCH_SIZE = 50
-
+# portfolio_type = "PUBLIC"
+portfolio_type = "PRIVATE"
 
 trader_links_batch = []
 trader_metrics_batch = []
